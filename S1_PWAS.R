c74628d370b73a4dcd1a5aa1e1693ec25cbc915c
chunshen617 <48015870+chunshen617@users.noreply.github.com>
2024-07-07 23:17:57 +0100

    Update S1_PWAS.R

--- S1_PWAS.R
+++ S1_PWAS.R
@@ -1,17 +1,18 @@
+library(parallel)
 library(data.table)
 library(RNOmni)
-library(foreach)
-library(doParallel)
 
 ##Load data
 data_prot <- fread('olink_ins0_cov.csv',data.table = F) #plasma protein data with covariates
 protName <- colnames(data_prot)[2:2921]
 protName2 <- chartr('-','_',protName)
 
-##Model1:controlling for batch, time gap between blood collection and protein measurement, age, sex and site
+##################
+# Simple model: controlling for age，sex，site，technical，first 20 genetic PCs
 model1logistic <- function(data,protName,phenotype){
   data[,protName] <- RankNorm(data[,protName])
-  f <- paste0(phenotype,'~',protName,'+age+sex+Batch+timeGap+site')
+  f <- paste0(phenotype,'~',protName,'+age+sex+Batch+timeGap+site+',
+              paste0("PC", 1:20, collapse = "+"))
   fit <- glm(as.formula(f),data=data,family=binomial)
   or <- exp(coef(fit))
   s <- summary(fit)
@@ -23,11 +24,13 @@ model1logistic <- function(data,protName,phenotype){
                        pval=s[["coefficients"]][2,4])
   return(result)
 }
-#Model2:controlling for batch, time gap between blood collection and protein measurement, age, sex and site,
-#ethnicity, qualification, smoking, alcohol intake, TDI, and BMI
+
+# Full model:  controlling for age, sex, site, batch, time gap, 20 PC, ethnicity, education level, 
+#household income, smoking, alcohol consumption, BMI
 model2logistic <- function(data,protName,phenotype){
   data[,protName] <- RankNorm(data[,protName])
-  f <- paste0(phenotype,'~',protName,'+age+sex+Batch+timeGap+ethnicity+edu_4c+smokeNow+alc_2c+bmi_3c+inc_2c+site')
+  f <- paste0(phenotype,'~',protName,'+age+sex+Batch+timeGap+eth2+edu_4c+smokeNow+alc_2c+bmi+inc_2c+site+',
+              paste0("PC", 1:20, collapse = "+"))
   fit <- glm(as.formula(f),data=data,family=binomial)
   or <- exp(coef(fit))
   s <- summary(fit)
@@ -40,56 +43,66 @@ model2logistic <- function(data,protName,phenotype){
   return(result)
 }
 
-##setup parallel backend to use many processors
-registerDoParallel(20)
-
-##perform analysis
-Result_SI_m1 <- foreach(i=1:length(protName), .combine='rbind') %dopar% {
-  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site'))
-  dat_t <- subset(timeGap,select = c('eid',protName[i]))
+##################
+# SI, simple model
+SI.model1 <- mclapply(1:length(protName2), function(i) {
+  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site',paste0('PC',1:20)))
+  dat_t <- subset(timeGap,select = c('ID',protName[i]))
   colnames(dat_t)[2] <- 'timeGap'
-  dat2 <- merge(dat,dat_t,by.x='ID',by.y='eid')
+  dat2 <- merge(dat,dat_t,by='ID')
   dat2 <- na.omit(dat2)
   
-  r <- model1logistic(dat2,protName2[i],'SI_2c')
-  r
-}
-Result_LO_m1 <- foreach(i=1:length(protName), .combine='rbind') %dopar% {
-  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site'))
-  dat_t <- subset(timeGap,select = c('eid',protName[i]))
+  model1logistic(dat2,protName2[i],'SI_2c')
+}, mc.cores = 10)
+
+SI_model1 <- do.call(rbind,SI.model1)
+write.csv(SI_model1,file = 'Result_SI_protein_assoc_M1.csv')
+
+##################
+# LO, simple model
+LO.model1 <- mclapply(1:length(protName2), function(i) {
+  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site',paste0('PC',1:20)))
+  dat_t <- subset(timeGap,select = c('ID',protName[i]))
   colnames(dat_t)[2] <- 'timeGap'
-  dat2 <- merge(dat,dat_t,by.x='ID',by.y='eid')
+  dat2 <- merge(dat,dat_t,by='ID')
   dat2 <- na.omit(dat2)
   
-  r <- model1logistic(dat2,protName2[i],'LO_2c')
-  r
-}
+  model1logistic(dat2,protName2[i],'LO_2c')
+}, mc.cores = 10)
 
-Result_SI_m2 <- foreach(i=1:length(protName), .combine='rbind') %dopar% {
+LO_model1 <- do.call(rbind,LO.model1)
+write.csv(LO_model1,file = 'Result_LO_protein_assoc_M1.csv')
+
+##################
+# SI, full model
+SI.model2 <- mclapply(1:length(protName2), function(i) {
   dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site',
-                                     'ethnicity','edu_4c','smokeNow','alc_2c','bmi_3c','inc_2c'))
-  dat_t <- subset(timeGap,select = c('eid',protName[i]))
+                                     'eth2','edu_4c','smokeNow','alc_2c','bmi','inc_2c',
+                                     paste0('PC',1:20)))
+  dat_t <- subset(timeGap,select = c('ID',protName[i]))
   colnames(dat_t)[2] <- 'timeGap'
-  dat2 <- merge(dat,dat_t,by.x='ID',by.y='eid')
+  dat2 <- merge(dat,dat_t,by='ID')
   dat2 <- na.omit(dat2)
   
-  r <- model2logistic(dat2,protName2[i],'SI_2c')
-  r
-}
-Result_LO_m2 <- foreach(i=1:length(protName), .combine='rbind') %dopar% {
-  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site',
-                                     'ethnicity','edu_4c','smokeNow','alc_2c','bmi_3c','inc_2c'))
-  dat_t <- subset(timeGap,select = c('eid',protName[i]))
-  colnames(dat_t)[2] <- 'timeGap'
-  dat2 <- merge(dat,dat_t,by.x='ID',by.y='eid')
-  dat2 <- na.omit(dat2)
-  
-  r <- model2logistic(dat2,protName2[i],'LO_2c')
-  r
-}
+  model2logistic(dat2,protName2[i],'SI_2c')
+}, mc.cores = 10)
 
-##save results
-write.csv(Result_SI_m1, file = 'Result_SI_protein_assoc_M1.csv')
-write.csv(Result_LO_m1, file = 'Result_LO_protein_assoc_M1.csv')
-write.csv(Result_SI_m2, file = 'Result_SI_protein_assoc_M2_noIRN.csv')
-write.csv(Result_LO_m2, file = 'Result_LO_protein_assoc_M2_noIRN.csv')
+SI_model2 <- do.call(rbind,SI.model2)
+write.csv(SI_model2,file = 'Result_SI_protein_assoc_M2.csv')
+
+##################
+# LO, full model
+LO.model2 <- mclapply(1:length(protName2), function(i) {
+  dat <- subset(data_prot,select = c('ID',protName2[i],'SI_2c','LO_2c','Batch','age','sex','site',
+                                     'eth2','edu_4c','smokeNow','alc_2c','bmi','inc_2c',
+                                     paste0('PC',1:20)))
+  dat_t <- subset(timeGap,select = c('ID',protName[i]))
+  colnames(dat_t)[2] <- 'timeGap'
+  dat2 <- merge(dat,dat_t,by='ID')
+  dat2 <- na.omit(dat2)
+  
+  model2logistic(dat2,protName2[i],'LO_2c')
+}, mc.cores = 8)
+
+LO_model2 <- do.call(rbind,LO.model2)
+write.csv(LO_model2,file = 'Result_LO_protein_assoc_M2.csv')
