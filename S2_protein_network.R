253a3bb7d392df33a9866a0c8dd0770f1af5f14a
chunshen617 <48015870+chunshen617@users.noreply.github.com>
2024-07-07 23:27:47 +0100

    Update S2_protein_network.R

--- S2_protein_network.R
+++ S2_protein_network.R
@@ -10,7 +10,7 @@ smiss <- apply(data_prot[,2:2921],1,function(x) length(which(is.na(x)==TRUE))/nc
 data_prot2 <- data_prot[smiss<0.5,pmiss<0.5]#sample N=46850
 
 #permutation
-prot_dat <- as.matrix(data_prot2[,2:2921])
+prot_dat <- as.matrix(t(data_prot2[,2:2921]))
 prot_impute <- impute.knn(prot_dat,k = 10, rowmax = 0.5, colmax = 0.5, maxp = 1500, rng.seed=12345)
 dat_impute <- as.data.frame(prot_impute$data)
 
