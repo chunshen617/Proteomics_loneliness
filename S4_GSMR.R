06495a67e5c284a1700192e9689e0181207f2a61
chunshen617 <48015870+chunshen617@users.noreply.github.com>
2024-09-08 00:15:06 +0100

    Update S4_GSMR.R

--- S4_GSMR.R
+++ S4_GSMR.R
@@ -1,5 +1,11 @@
-#GSMR, using loneliness to proteins as an example
-#GWAS summary statistics of loneliness (calculated by PLINK2)
+# GSMR analysis, using loneliness to proteins as an example
+# Chun Shen, 2024
+
+# Load R packages
+library(gsmr2)
+library(data.table)
+
+#LO gwas
 LO_gwas <- fread('LO_GWAS_plink2.csv')
 #significant threshold
 LO_gwas_sig <- LO_gwas[LO_gwas$P<1e-6,]
@@ -13,20 +19,21 @@ LO_gwas_sig$A2 <- ifelse(LO_gwas_sig$A1==LO_gwas_sig$REF,'ALT','REF')
 LO_gwas_sig$noneffect[LO_gwas_sig$A2=='ALT'] <- LO_gwas_sig$ALT[LO_gwas_sig$A2=='ALT']
 LO_gwas_sig$noneffect[LO_gwas_sig$A2=='REF'] <- LO_gwas_sig$REF[LO_gwas_sig$A2=='REF']
 
-#clump
+#clumping
 LO_snp_clump <- ld_clump_local(data.frame(rsid = LO_gwas_sig$SNP,pval = LO_gwas_sig$P),
                                clump_kb=10000,
                                clump_r2=0.001,
                                clump_p = 1,
-                               bfile = '/home1/shenchun/DATA/Genome1000_EUR/EUR',
-                               plink_bin = '/home1/shenchun/software/plink_1.9/plink')
+                               bfile = '/Genome1000_EUR/EUR',
+                               plink_bin = '/software/plink_1.9/plink')
 
 LO_exp_clump <- LO_gwas_sig[which(LO_gwas_sig$SNP %in% LO_snp_clump$rsid),]
 
-#protein gwas
+# protein GWAS folder
 gwas_path <- '/home1/prot_GCTA_result/'
 
-Result_LO_m2 <- fread('/Result_LO_protein_assoc_M2.csv',data.table = F)
+#significant proteins associated with loneliness
+Result_LO_m2 <- fread('Result_LO_protein_assoc_M2.csv',data.table = F)
 bonf_LO_m2 <- Result_LO_m2[Result_LO_m2$pval<0.025/nrow(Result_LO_m2),]
 protName <- bonf_LO_m2$V1
 
