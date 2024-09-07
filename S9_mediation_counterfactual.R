31c358bae7fe8b31e0b8fdb50bb4906f3eb99445
chunshen617 <48015870+chunshen617@users.noreply.github.com>
2024-09-08 00:49:26 +0100

    Update S9_mediation_counterfactual.R

--- S9_mediation_counterfactual.R
+++ S9_mediation_counterfactual.R
@@ -1,4 +1,7 @@
-#Mediation analysis caculated by counterfactual-based mediation analysis, using CVD as an example
+# Counterfactual-based mediation analysis, using CVD as an example
+# Chun Shen, 2024
+
+# Load R packages
 library('data.table')
 library('lubridate')                                        
 library('RNOmni')
@@ -7,16 +10,22 @@ library('foreach')
 library('doParallel')
 library('timereg')
 
-##Load data
-data_prot <- fread('olink_ins0_cov.csv',data.table = F) #plasma protein data with covariates
+###Load data
+# time gap: N participants * M proteins
+timeGap <- fread('olink_timeGap.csv')
+colnames(timeGap)[1] <- 'ID'
+#protein data and other covariates
+data_prot <- fread('protein_UKB.csv',data.table = F)
 protName <- colnames(data_prot)[2:2921]
+protName2 <- chartr('-','_',protName)
+colnames(data_prot)[2:2921] <- protName2
 
-#MR significant proteins
-mr_toProt <- fread('Result_MR_toProt_ME.csv')
-fdrsig_mr_ivw <- mr_toProt[mr_toProt$pfdr_com<0.05,] #9 protein and protein modules
-prot_use <- fdrsig_mr_ivw$outcome
-#extract protein data
-data_prot_use <- data_prot[,c('ID',prot_use,'SI_2c','LO_2c','age','sex','site','ethnicity','edu_4c','smokeNow','inc_2c','alc_2c','bmi_3c')]
+# MR sig proteins
+sig_toprot <- fread('Result_IVW_sig_toProt.csv')
+prot_used <- sig_toprot$outcome
+
+data_prot_use <- data_prot[,c('ID',prot_used,'SI_2c','LO_2c','sex','eth2','age','site','Batch',
+                              'edu_4c','smokeNow','alc_2c','bmi','inc_2c',paste0("PC", 1:20))]
 
 #Health outcome data from UKB first-occurence
 phenoData <- fread('COXphenoData_raw.csv')
@@ -58,56 +67,73 @@ Data_merge3$outcome <- ifelse(is.na(Data_merge3$CVD1Date)==FALSE,1,0)
 Data_merge3$fultime <- timedif(Data_merge3$CVD1Date,Data_merge3$AttendDate,Data_merge3$DeathDate,'2022-11-30')
 
 ##############
-registerDoParallel(20)
+registerDoParallel(10)
 
-Result_CVD_cmed1 <- foreach(i=1:length(prot_use), .combine='rbind') %dopar% {
-  #simple model: adjust for age, sex, site
-  dat_simple <- na.omit(subset(Data_merge3,select=c('ID',prot_use[i],'outcome','fultime','LO_2c','age','sex')))
-  dat_simple$exposure <- RankNorm(dat_simple[,2])
+Result_CVD_cmed2 <- foreach(i=1:length(prot_used), .combine='rbind') %dopar% {
+  # Covariate
+  dat <- subset(Data_merge3,select=c('ID',prot_used[i],'outcome','fultime','LO_2c','age','sex','site','Batch',
+                                           'eth2','edu_4c','smokeNow','alc_2c','bmi','inc_2c',
+                                           paste0("PC", 1:20)))
+  dat_t <- subset(timeGap,select = c('ID',prot_used[i]))
+  colnames(dat_t)[2] <- 'timeGap'
+  mydata <- na.omit(merge(dat,dat_t,by='ID'))
+  mydata$mediator <- RankNorm(mydata[,2])
+
+  # category
+  mydata$sex <- as.factor(mydata$sex)
+  mydata$eth2 <- as.factor(mydata$eth2)
+  mydata$edu_4c <- as.factor(mydata$edu_4c)
+  mydata$inc_2c <- as.factor(mydata$inc_2c)
+  mydata$smokeNow <- as.factor(mydata$smokeNow)
+  mydata$alc_2c <- as.factor(mydata$alc_2c)
+  mydata$site <- as.factor(mydata$site)
+  mydata$Batch <- as.factor(mydata$Batch)
   
   ##############
-  #Aalen model
-  #Step 1: protein~LO_2c+covs
-  dat_simple$LOtemp <- dat_simple$LO_2c
-  fitM_1 <- lm(exposure ~ factor(LOtemp)+age+sex, data=dat_simple) 
-  fitMvar_1 <- summary(fitM_1)$sigma^2
+  #Step 1: Mediator (protein) ~ LO+covs
+  mydata$LOtemp <- mydata$LO_2c
+  f1 <- paste0('mediator ~ LOtemp+age+sex+eth2+edu_4c+smokeNow+inc_2c+alc_2c+bmi+timeGap+site+Batch+',paste0("PC", 1:20, collapse = "+"))
+  fitM <- lm(as.formula((f1)), data=mydata) 
+  fitMvariance <- summary(fitM)$sigma^2
   
   #Step 2: construct new variables id and LO_2c where the last corresponds to the value of the exposure relative to the indirect path.
-  dat_simple1 <- dat_simple
-  dat_simple2 <- dat_simple
-  dat_simple1$LOstar <- dat_simple$LO_2c
-  dat_simple2$LOstar <- 1-dat_simple$LO_2c
-  newDat_simple <- rbind(dat_simple1, dat_simple2)
+  mydata1 <- mydata
+  mydata2 <- mydata
+  mydata1$LOstar <- mydata$LO_2c
+  mydata2$LOstar <- 1-mydata$LO_2c
+  newdata <- rbind(mydata1,mydata2)
   
   #Step 3: compute weights
-  newDat_simple$LOtemp <- newDat_simple$LO_2c
-  tempDir_1 <- dnorm(newDat_simple$exposure, 
-                     mean=predict(fitM_1, type = "response", newdata=newDat_simple), 
-                     sd=sqrt(fitMvar_1) )
-  newDat_simple$LOtemp <- newDat_simple$LOstar
-  tempIndir_1 <- dnorm(newDat_simple$exposure, 
-                       mean=predict(fitM_1, type = "response", newdata=newDat_simple), 
-                       sd=sqrt(fitMvar_1) )
-  newDat_simple$weight <- tempIndir_1/tempDir_1
-  
-  #Step 4: the MSM model for direct and indirect effects can be estimated
-  #Aalens Model
-  fitYaalen_1 <- aalen(Surv(fultime, outcome)~const(factor(LO_2c))+const(factor(LOstar))+const(age)+const(sex), 
-                       data=newDat_simple, weights=newDat_simple$weight, clusters=newDat_simple$eid) 
-  #clean results
-  sumAalen_1 <- as.data.frame(capture.output(summary(fitYaalen_1)))
-  sumAalen_2 <- sumAalen_1[17:20,]
+  newdata$LOtemp <- newdata$LO_2c
+  tempDir <- dnorm(newdata$mediator,
+                  mean=predict(fitM,type='response',newdata=newdata),
+                  sd=sqrt(fitMvariance))
+  newdata$LOtemp <- newdata$LOstar
+  tempIndir <- dnorm(newdata$mediator,
+                    mean=predict(fitM,type='response',newdata=newdata),
+                    sd=sqrt(fitMvariance))
+  newdata$weightM <- tempIndir/tempDir
+
+  #Step 4: Aalens model
+  f2 <- 'Surv(fultime, outcome) ~ const(factor(LO_2c))+const(factor(LOstar))+const(age)+const(sex)+const(eth2)+const(edu_4c)+const(smokeNow)+const(inc_2c)+const(alc_2c)+const(bmi)+const(timeGap)+const(site)'
+  fitYaalen <- aalen(as.formula(f2),data=newdata,weights=newdata$weightM,clusters=newdata$ID)
+
+  sumAalen_1 <- as.data.frame(capture.output(summary(fitYaalen)))
+  sumAalen_2 <- sumAalen_1[17:52,]
   sumAalen_3 <- strsplit(sumAalen_2, " ")
   filtered_data <- lapply(sumAalen_3, function(x) x[x != ""])
   max_cols <- max(sapply(filtered_data, length))
   padded_data <- lapply(filtered_data, function(x) c(x, rep(NA, max_cols - length(x))))
-  result_Aalen_1 <- as.data.frame(do.call(rbind, padded_data))
+  result_Aalen <- as.data.frame(do.call(rbind, padded_data))
   
   #direct and indirect effect
-  result <- data.frame(dirEff=result_Aalen_1[1,2],dirSE=result_Aalen_1[1,3],dirRSE=result_Aalen_1[1,4],dirZ=result_Aalen_1[1,5],dirP=result_Aalen_1[1,6],
-                       indirEff=result_Aalen_1[2,2],indirSE=result_Aalen_1[2,3],indirRSE=result_Aalen_1[2,4],indirZ=result_Aalen_1[2,5],indirP=result_Aalen_1[2,6],)
-  
+  result <- data.frame(protname = prot_used[i],disease = 'CVD',
+                      dirEff=result_Aalen[1,2],dirRSE=result_Aalen[1,4],dirZ=result_Aalen[1,5],dirP=result_Aalen[1,6],
+                      indirEff=result_Aalen[2,2],indirRSE=result_Aalen[2,4],indirZ=result_Aalen[2,5],indirP=result_Aalen[2,6])
+
   result
 }
 
-save(Result_CVD_cmed1,file='Result_causalMediation_CVD.RData')
+write.csv(Result_CVD_cmed2, file='Result_causalMediation_CVD_M2_v2.csv', row.names = F)
+
+
